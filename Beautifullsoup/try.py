8438097b020af6f578b9ef1d1704a6e18c1c4985
Michael <michaelmomule@gmail.com>
2023-05-03 14:43:22 +0300

    find jobs.try webscrap

--- Beautifullsoup/try.py
+++
@@ -1,6 +0,0 @@
-sentences = ["This is the first sentence.", "This is the second sentence.", "This is the third sentence."]
-
-for sentence in sentences:
-    words = sentence.split()
-    for word in words:
-        print(word)
